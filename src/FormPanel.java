5ca804c30168021b96e42954470d463d0a0f28b6
blazo <01178624@pw.edu.pl>
2024-02-29 17:18:05 +0100

    dodano metodę createText oraz podzielono dodatkowo używając BorderLayout

--- src/FormPanel.java
+++ src/FormPanel.java
@@ -4,25 +4,57 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class FormPanel extends JPanel implements ActionListener {
+
     BoxLayout boxLay;
     CustomTextField textField1;
     CustomTextField textField2;
     CustomButton submitButton;
+    JPanel centralPanel;
+    JPanel leftPanel;
+    JPanel rightPanel;
+    JPanel bottomPanel;
 
     FormPanel() {
-        boxLay = new BoxLayout(this, BoxLayout.Y_AXIS);
-        setLayout(boxLay);
-        textField1 = new CustomTextField("Imię",20);
-        add(textField1);
-        textField2 = new CustomTextField("Nazwisko",20);
-        add(textField2);
+
+        setLayout(new BorderLayout());
+
+        centralPanel = new JPanel();
+        leftPanel = new JPanel();
+        rightPanel = new JPanel();
+        bottomPanel = new JPanel();
+
+        bottomPanel.setBackground(Color.BLACK);
+        centralPanel.setBackground(new Color(0,56,212));
+        leftPanel.setBackground(Color.CYAN);
+
+        centralPanel.setPreferredSize(new Dimension(600,600));
+        leftPanel.setPreferredSize(new Dimension(100,600));
+        rightPanel.setPreferredSize(new Dimension(100,600));
+        bottomPanel.setPreferredSize(new Dimension(800,35));
+
+        add(centralPanel,BorderLayout.CENTER);
+        add(leftPanel,BorderLayout.WEST);
+        add(rightPanel,BorderLayout.EAST);
+        add(bottomPanel,BorderLayout.SOUTH);
+
+        boxLay = new BoxLayout(centralPanel, BoxLayout.Y_AXIS);
+        centralPanel.setLayout(boxLay);
+
+        String test = createText("Imię",centralPanel);
+        String test1 = createText("Nazwisko",centralPanel);
+
+        centralPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
         submitButton = new CustomButton("SUBMIT");
         submitButton.setAlignmentX(0.5f);
-        add(submitButton);
+        bottomPanel.add(submitButton);
         submitButton.addActionListener(this);
+    }
 
-
-
+    private String createText(String text, JPanel jPanel){
+        CustomTextField textField = new CustomTextField(text);
+        jPanel.add(textField);
+        jPanel.add(Box.createVerticalStrut(20));
+        return text;
     }
 
     @Override
