7493aded95a5ff2014fd1e629446cf9d6364b2e9
wzochows <wiola062210@gmail.com>
2024-02-29 15:25:44 +0100

    Dodane komponenty do StartPanel, dodany IO Exception

--- src/CustomFrame.java
+++ src/CustomFrame.java
@@ -1,12 +1,13 @@
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
 
 public class CustomFrame extends JFrame {
     CardLayout cards;
     StartPanel mainPage;
     FormPanel formPage;
 
-    CustomFrame() {
+    CustomFrame() throws IOException {
         setSize(800,700);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("FORMULARZ");
--- src/StartPanel.java
+++ src/StartPanel.java
@@ -1,23 +1,54 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class StartPanel extends JPanel implements ActionListener {
     BoxLayout boxLay;
+    JLabel logoLabel;
+    JTextArea welcomeInfo;
+    CustomButton cb;
 
-    public StartPanel() {
+    public StartPanel() throws IOException {
         boxLay = new BoxLayout(this, BoxLayout.Y_AXIS);
         setLayout(boxLay);
 
-        CustomLabel cl = new CustomLabel("Witaj! \nJeśli chcesz wypełnić formularz swoimi danymi, kliknij przycisk Register");
-        CustomButton cb = new CustomButton("REGISTER");
+        //label for image TRZEBA WYCENTROWAC, KOLOR MUSI byc jak tlo na zdjeciu
+        // nie wiem czy te zdejcie czy jakies inne jak chcecie ICONIFY stronka cos wybrac
+        // customButton trzeba zrobic ladniejszy zeby wszystkie colorki ze soba pasowaly
+        // no i ten srodkowy areatrest ma kolor rozowy tylko po to by widac jego granice, trzeba jakos to uporzadkowac
+
+        BufferedImage userLOGO = ImageIO.read(new File("resources\\user.png"));
+        Image newImage = userLOGO.getScaledInstance(200, 200, Image.SCALE_DEFAULT);
+
+        ImageIcon logoIcon = new ImageIcon(newImage);
+
+        logoLabel = new JLabel(logoIcon);
+
+
+
+
+        // info about joining
+        welcomeInfo = new JTextArea("Witaj! \nJeśli chcesz wypełnić formularz swoimi danymi, kliknij przycisk Register");
+        welcomeInfo.setBackground(Color.PINK);
+        welcomeInfo.setEditable(false);
+        welcomeInfo.setLineWrap(true);
+        welcomeInfo.setFocusable(false);
+
+        // register button
+        cb = new CustomButton("REGISTER");
         cb.addActionListener(this);
 
-        add(cl);
+        add(logoLabel);
+        add(welcomeInfo);
         add(cb);
 
     }
+
 
 
     @Override
@@ -28,4 +59,5 @@ public class StartPanel extends JPanel implements ActionListener {
             cardLayout.show(container, "formSheet");
         }
     }
+
 }
