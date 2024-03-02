cd2654881a73615bd82dea179fcb55d1b6406789
blazo <01178624@pw.edu.pl>
2024-03-02 17:22:35 +0100

    dodano gettery

--- src/User.java
+++ src/User.java
@@ -2,13 +2,13 @@ public class User {
 
     private final String name;
     private final String surname;
-    private final long PESEL;
+    private final String PESEL;
     private final String gender;
     private final String birthDate;
     private final String email;
     private final int phoneNumber;
 
-    User(String name, String surname, long PESEL, String birthDate, String email, int phoneNumber, String gender){
+    User(String name, String surname, String PESEL, String birthDate, String email, int phoneNumber, String gender){
         this.name = name;
         this.surname = surname;
         this.PESEL = PESEL;
@@ -16,5 +16,33 @@ public class User {
         this.birthDate = birthDate;
         this.email = email;
         this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getPESEL() {
+        return PESEL;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
     }
 }
